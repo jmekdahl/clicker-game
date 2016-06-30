b48ad9b8089f9d56784bc65ef6c2772b32b0cea6
James Ekdahl <jmekdahl@gmail.com>
2016-06-30 11:12:09 -0500

    no message

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <script src="http://code.jquery.com/jquery-3.0.0.min.js"></script
+        <script src="http://code.jquery.com/jquery-3.0.0.min.js"></script>
         <script src="/clicker-game/assets/vendor/rivets/rivets.0.8.0.bundled.min.js"></script>
         <script src="/clicker-game/assets/js/spellClicker.js"></script>
     </head>
