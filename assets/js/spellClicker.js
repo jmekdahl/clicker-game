23aa32fcd8f5da89272f950926a5ae6158cf2c30
James Ekdahl <jmekdahl@gmail.com>
2016-06-30 12:34:10 -0500

    Added More Spell Icons and started game loop

--- assets/js/spellClicker.js
+++ assets/js/spellClicker.js
@@ -11,16 +11,33 @@ SpellClicker.Spell = function(name, damage, image){
 };
 
 SpellClicker.Spells = {
-    'lightning1': new SpellClicker.Spell('Lightning Bolt', 10, "lighting-blue-1.png")
+    'lightning1': new SpellClicker.Spell('Lightning Bolt', 10, "lighting-blue-1.png"),
+    'frost1': new SpellClicker.Spell('Ice Lance', 2, "ice-blue-1.png"),
+    'fire1': new SpellClicker.Spell('Lightning Bolt', 15, "fireball-red-1.png")
 };
 
 SpellClicker.game.spellQueue = [
         Object.create(SpellClicker.Spells.lightning1)
 ];
 
+SpellClicker.getSpell = function(obj){
+    var keys = Object.keys(obj);
+    return obj[keys[ keys.length * Math.random() << 0]];
+}
+
+SpellClicker.tick = function(){
+    if(SpellClicker.game.SpellQueue.length <= 6 ){
+        var newSpell = SpellClicker.getSpell(SpellClicker.Spells);
+        console.log(newSpell);
+    }
+};
+
+SpellClicker.loop = setInterval(SpellClicker.tick, 1000);
+
 $(document).ready( function() {
     rivets.bind($('#game'), SpellClicker.game);
 });
+
 
 
 // rivets.formatters.propertyList = function(obj){
